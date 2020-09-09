1ee5d6d8a88febff81e98bb7f76c6c1190f748df
Abel Masila <abelmasila@gmail.com>
2020-09-09 19:02:30 +0300

    Added checkout route and router too

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Checkout from "./pages/Checkout";
 import "./App.css";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
