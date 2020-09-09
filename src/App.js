4be8161aaacdf150a8f6ef571bf829a0255c1a02
Abel Masila <abelmasila@gmail.com>
2020-09-09 06:06:12 +0300

    working on banner

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
+import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      {/* Home */}
-      {/*  */}
+      <Home />
     </div>
   );
 }
