8aa118e57735046567192879f5215fcda7761865
Abel Masila <abelmasila@gmail.com>
2020-09-09 04:46:00 +0300

    added firebase config

---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBqYzEnUH9HcMkBnp3XRq1rY6trvZ76gg0",
+  authDomain: "clone-27420.firebaseapp.com",
+  databaseURL: "https://clone-27420.firebaseio.com",
+  projectId: "clone-27420",
+  storageBucket: "clone-27420.appspot.com",
+  messagingSenderId: "86091796046",
+  appId: "1:86091796046:web:91184297ac950581373d1f",
+  measurementId: "G-JLZVKT8L0C",
+};
